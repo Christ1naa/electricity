8a822340ce2d0ce664910be1c750067a084cd11e
Christ1naa <168696012+Christ1naa@users.noreply.github.com>
2025-06-08 19:41:02 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  tariffs: {
+    day: 3.5, // грн
+    night: 2.0 // грн
+  },
+  penalty: {
+    day: 100, // квт
+    night: 80 // квт
+  },
+  initialMeters: {
+    "ABC123": { day: 1000, night: 800 }
+  }
+};
