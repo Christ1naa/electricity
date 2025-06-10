4040858206998991aac0c272a441cb128e255ca0
Christ1naa <168696012+Christ1naa@users.noreply.github.com>
2025-06-10 17:30:31 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getDatabase, ref, set, get, child, push } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+import { getDatabase, ref, set, get, push } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCuZfuqlFhXcn8bb_3YbETGhPiFhA1FfXI",
   authDomain: "luchylnyk-app.firebaseapp.com",
+  databaseURL: "https://luchylnyk-app-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "luchylnyk-app",
-  storageBucket: "luchylnyk-app.firebasestorage.app",
+  storageBucket: "luchylnyk-app.appspot.com",
   messagingSenderId: "538399551820",
   appId: "1:538399551820:web:76a0da38a6a8df8d301fe9",
-  measurementId: "G-T29EQTSP62",
-  databaseURL: "https://luchylnyk-app-default-rtdb.europe-west1.firebasedatabase.app/"
+  measurementId: "G-T29EQTSP62"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-export { db, ref, set, get, child, push };
+
+export { db, ref, set, get, push };
