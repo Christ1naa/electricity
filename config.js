1b2eb9793f4d39a20dec5cb815e8ebcc9c737aa6
Christ1naa <168696012+Christ1naa@users.noreply.github.com>
2025-06-10 14:22:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
   tariffs: {
-    day: 3.5, // грн
-    night: 2.0 // грн
+    day: 3.5,
+    night: 2.0
   },
   penalty: {
-    day: 100, // квт
-    night: 80 // квт
+    day: 100,
+    night: 80
   },
   initialMeters: {
     "ABC123": { day: 1000, night: 800 }
